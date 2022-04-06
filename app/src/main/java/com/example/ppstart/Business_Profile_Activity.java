7ec379aa82dd427efaa8da858093781496a329fa
stephencdevaney <stephendevaney@rocketmail.com>
2022-04-06 16:54:53 -0500

    Inserted Business_Profile_Activity Nav Bar

--- app/src/main/java/com/example/ppstart/Business_Profile_Activity.java
+++ app/src/main/java/com/example/ppstart/Business_Profile_Activity.java
@@ -9,11 +9,17 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Business_Profile_Activity extends AppCompatActivity {
 
     private TextView owner_username_view;
+    private Button guest_btn;
+    private BottomNavigationView bottom_nav_menu;
     private FragmentManager fragment_manager;
     private int owner_Id;
     private int supporter_Id;
@@ -26,6 +32,8 @@ public class Business_Profile_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_profile);
         owner_username_view = findViewById(R.id.owner_username);
+        guest_btn = findViewById(R.id.buss_pro_guest_btn);
+        bottom_nav_menu = findViewById(R.id.profile_bottem_nav);
 
         databaseHelper = new DatabaseHelper(Business_Profile_Activity.this);
         SQLiteDatabase db = databaseHelper.getReadableDatabase();
@@ -41,8 +49,16 @@ public class Business_Profile_Activity extends AppCompatActivity {
                 // provide supporter functionality
                 if (bundle.containsKey("supporter_id") && bundle.containsKey("supporter_username")){
                     supporter_Id = bundle.getInt("supporter_id");
-                    if(supporter_Id == -1) owner_username_view.setText("Hello Guest!");
-                    else owner_username_view.setText("Hello, " + bundle.getString("supporter_username") + "!");
+                    if(supporter_Id == -1) {
+                        owner_username_view.setVisibility(View.GONE);
+                        guest_btn.setVisibility(View.VISIBLE);
+                        guestButtonSetup();
+                    }
+                    else {
+                            owner_username_view.setVisibility(View.VISIBLE);
+                            owner_username_view.setText("Hello, " + bundle.getString("supporter_username") + "!");
+                            guest_btn.setVisibility(View.GONE);
+                        }
                 }
                 else{ // provide owner functionality
                     //create cursor to move query the db and move through the query
@@ -60,12 +76,15 @@ public class Business_Profile_Activity extends AppCompatActivity {
                                 if (owner_Id == owner_cursor.getInt(owner_id_index)) break;
                                 owner_cursor.moveToNext();
                             }
+                            owner_username_view.setVisibility(View.VISIBLE);
                             owner_username_view.setText("Hello, " + owner_cursor.getString(fname_index) + " " + owner_cursor.getString(lname_index) + "!");
+                            guest_btn.setVisibility(View.GONE);
                         }
                     }
                 }
                 profile_cursor = db.rawQuery("SELECT * FROM profile WHERE owner_id=" + owner_Id,null);
                 createAboutFragmentManager();
+                BottomNavMenuSetup();
             }
         }
     }
@@ -92,6 +111,38 @@ public class Business_Profile_Activity extends AppCompatActivity {
         fragment_manager.beginTransaction().replace(R.id.business_fragment_view, business_profile_text.class, fragment_info).setReorderingAllowed(true).addToBackStack("name").commit();
     }
 
+    private void guestButtonSetup(){
+        //toolbar button
+        guest_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent logout = new Intent(Business_Profile_Activity.this, MainActivity.class);
+                logout.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(logout);
+            }
+        });
+    }
+
+    private void BottomNavMenuSetup(){
+        bottom_nav_menu.setSelectedItemId(R.id.about_button);
+        bottom_nav_menu.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.about_button:
+                    break;
+                case R.id.inventory_button:
+                    break;
+                case R.id.hours_button:
+                    break;
+                case R.id.layout_button:
+                    break;
+                case R.id.faq_button:
+            }
+            return true;
+        });
+    }
+
     @Override
-    public void onBackPressed() { super.onBackPressed(); }
+    public void onBackPressed() {
+    //    super.onBackPressed();
+    }
 }
