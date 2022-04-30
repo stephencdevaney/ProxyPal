89a5ed2ef2589a22f1197bcb8084032d374bc508
stephencdevaney <stephendevaney@rocketmail.com>
2022-04-30 10:29:14 -0500

    Update item

--- app/src/main/java/com/example/ppstart/item.java
+++ app/src/main/java/com/example/ppstart/item.java
@@ -2,8 +2,19 @@ package com.example.ppstart;
 
 public class item {
     //basic data structure, formatted in xml format file
-    private String item;
-    public item(String itemName){
-        this.item = itemName;
+    private String name;
+    private String price;
+    private String image;
+
+
+    public void setName(String itemName){
+        this.name = itemName
     }
+    public void setPrice(String itemPrice) {this.price = itemPrice;}
+    public void setImage(String itemImage) {this.image = itemImage;}
+
+
+    public String getName() { return name;}
+    public String getPrice() {return price;}
+    public String getImage() {return image;}
 }
